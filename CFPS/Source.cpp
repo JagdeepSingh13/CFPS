08c44705ed7be94bc3bf79906329fc91ffd607ae
JagdeepSingh13 <jagdeeps3105@gmail.com>
2025-11-27 19:03:40 +0530

    changed to olc classes

--- CFPS/Source.cpp
+++ CFPS/Source.cpp
@@ -4,142 +4,95 @@
 #include<vector>
 #include<utility>
 #include<algorithm>
+#include <olcConsoleGameEngine.h>
 using namespace std;
 
-int nScreenWidth = 120;
-int nScreenHeight = 40;
-
-float fPlayerX = 8.0f;
-float fPlayerY = 8.0f;
-// angle player is looking
-float fPlayerA = 0.0f;
-
-int nMapHeight = 16;
-int nMapWidth = 16;
-
-float fFOV = 3.14159 / 4.0;
-float fDepth = 16.0f;
-
-bool bJustShot = false;
-float fShotTimer = 0.0f;
-// float fShotCooldown = 0.25f;
-
-void shootRay(std::wstring& map) {
-	float fRayX = sinf(fPlayerA);
-	float fRayY = cosf(fPlayerA);
-
-	float dist = 0.0f;
-	while (dist < fDepth) {
-		dist += 0.1f;
-		
-		int nTestX = (int)(fPlayerX + fRayX * dist);
-		int nTestY = (int)(fPlayerY + fRayY * dist);
-
-		// test if ray is out of bounds
-		if (nTestX < 0 || nTestX >= nMapWidth || nTestY < 0 || nTestY >= nMapHeight) {
-			return;
-		}
-
-		if (map[nTestY * nMapWidth + nTestX] == '#') {
-			// break the wall
-			map[nTestY * nMapWidth + nTestX] = '.';
-			return;
-		}
+class Ultimate_FPS : public olcConsoleGameEngine {
+public:
+	Ultimate_FPS() {
+		m_sAppName = L"ULtimate FPS";
 	}
-}
+private:
+	int nMapWidth = 16;
+	int nMapHeight = 16;
 
-void drawMuzzleFlash(wchar_t* screen) {
-	int cx = nScreenWidth / 2;
-	int cy = nScreenHeight - 6;
-
-	wchar_t flare = 0x2588;
-
-	// 5×5 block (except corners)
-	for (int y = -2; y <= 2; y++) {
-		for (int x = -2; x <= 2; x++) {
-			if (abs(x) + abs(y) < 4)   // diamond shape
-				screen[(cy + y) * nScreenWidth + (cx + x)] = flare;
-		}
-	}
-}
-
-int main() {
-	// create screen buffer
-	wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight];
-	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
-	SetConsoleActiveScreenBuffer(hConsole);
-	DWORD dwBytesWritten = 0;
-
+	float fPlayerX = 8.0f;
+	float fPlayerY = 8.0f;
+	float fPlayerA = -3.14159f / 2.0f;
+	float fFOV = 3.14159f / 4.0f;
+	float fDepth = 16.0f;
+	float fSpeed = 5.0f;
 	std::wstring map;
 
-	map += L"################";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#...........##.#";
-	map += L"#...........##.#";
-	map += L"#...........##.#";
-	map += L"#...........##.#";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#........#######";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"################";
+	// bool bJustShot = false;
+	// float fShotTimer = 0.0f;
+protected:
+	virtual bool OnUserCreate() {
+		map.clear();
+		map += L"################";
+		map += L"#..............#";
+		map += L"#..............#";
+		map += L"#...........##.#";
+		map += L"#...........##.#";
+		map += L"#...........##.#";
+		map += L"#...........##.#";
+		map += L"#..............#";
+		map += L"#..............#";
+		map += L"#..............#";
+		map += L"#..............#";
+		map += L"#..............#";
+		map += L"#........#######";
+		map += L"#..............#";
+		map += L"#..............#";
+		map += L"################";
 
-	auto tp1 = chrono::system_clock::now();
-	auto tp2 = chrono::system_clock::now();
+		return true;
+	}
 
-	// game loop
-	while (1) {
-		tp2 = chrono::system_clock::now();
-		chrono::duration<float> elapsedTIme = tp2 - tp1;
-		tp1 = tp2;
-		float fElapsedTime = elapsedTIme.count();
+	virtual bool OnUserUpdate(float fElapsedTime) {
+#ifdef _DEBUG
+		if (m_bufScreen == nullptr) {
+			MessageBoxA(nullptr, "m_bufScreen is NULL in OnUserUpdate()", "Debug", MB_ICONERROR);
+			return false;
+		}
+#endif
 
-		// controls
-		if (GetAsyncKeyState((unsigned short)'A') & 0x8000)
-			fPlayerA -= (0.8f)* fElapsedTime;
-		
-		if (GetAsyncKeyState((unsigned short)'D') & 0x8000)
-			fPlayerA += (0.8f)* fElapsedTime;
+		// Handle CCW Rotation
+		if (m_keys[L'A'].bHeld)
+			fPlayerA -= (fSpeed * 0.5f) * fElapsedTime;
 
-		if (GetAsyncKeyState((unsigned short)'W') & 0x8000) {
-			fPlayerX += sinf(fPlayerA) * 5.0f * fElapsedTime;
-			fPlayerY += cosf(fPlayerA) * 5.0f * fElapsedTime;
+		// Handle CW Rotation
+		if (m_keys[L'D'].bHeld)
+			fPlayerA += (fSpeed * 0.5f) * fElapsedTime;
 
-			// collision detection
-			// inside of map is used to get the pos of player
-			if (map[(int)fPlayerY * nMapWidth + (int)fPlayerX] == '#') {
-				fPlayerX -= sinf(fPlayerA) * 5.0f * fElapsedTime;
-				fPlayerY -= cosf(fPlayerA) * 5.0f * fElapsedTime;
+		// Handle Forwards movement & collision
+		if (m_keys[L'W'].bHeld)
+		{
+			fPlayerX += sinf(fPlayerA) * fSpeed * fElapsedTime;;
+			fPlayerY += cosf(fPlayerA) * fSpeed * fElapsedTime;;
+			if (map.c_str()[(int)fPlayerX * nMapWidth + (int)fPlayerY] == '#')
+			{
+				fPlayerX -= sinf(fPlayerA) * fSpeed * fElapsedTime;;
+				fPlayerY -= cosf(fPlayerA) * fSpeed * fElapsedTime;;
 			}
 		}
 
-		if (GetAsyncKeyState((unsigned short)'S') & 0x8000) {
-			fPlayerX -= sinf(fPlayerA) * 5.0f * fElapsedTime;
-			fPlayerY -= cosf(fPlayerA) * 5.0f * fElapsedTime;
-
-			if (map[(int)fPlayerY * nMapWidth + (int)fPlayerX] == '#') {
-				fPlayerX += sinf(fPlayerA) * 5.0f * fElapsedTime;
-				fPlayerY += cosf(fPlayerA) * 5.0f * fElapsedTime;
+		// Handle backwards movement & collision
+		if (m_keys[L'S'].bHeld)
+		{
+			fPlayerX -= sinf(fPlayerA) * fSpeed * fElapsedTime;;
+			fPlayerY -= cosf(fPlayerA) * fSpeed * fElapsedTime;;
+			if (map.c_str()[(int)fPlayerX * nMapWidth + (int)fPlayerY] == '#')
+			{
+				fPlayerX += sinf(fPlayerA) * fSpeed * fElapsedTime;;
+				fPlayerY += cosf(fPlayerA) * fSpeed * fElapsedTime;;
 			}
 		}
 
-		fShotTimer += fElapsedTime;
-		bJustShot = false;
-		if ((GetAsyncKeyState((unsigned short)'Q') & 0x8000)) {
-			fShotTimer = 0.0f;
-			bJustShot = true;
-			shootRay(map);
-		}
-
-		for (int x = 0; x < nScreenWidth; x++) {
+		for (int x = 0; x < ScreenWidth(); x++) {
 			// we will send out 120 rays
 			// for each column, calculate projected ray angle into world space
-			float fRayAngle = (fPlayerA - fFOV / 2.0f) + ((float)x / (float)nScreenWidth) * fFOV;
+			float fRayAngle = (fPlayerA - fFOV / 2.0f) + ((float)x / (float)ScreenWidth()) * fFOV;
 
 			float fDistToWall = 0;
 			bool bHitWall = false;
@@ -191,8 +144,8 @@ int main() {
 			}
 			// calculate dist to ceiling and floor, to give illusion of further away
 			// as dist to wall inc., ceiling inc.
-			int nCeiling = (float)(nScreenHeight / 2.0) - nScreenHeight / ((float)fDistToWall);
-			int nFloor = nScreenHeight - nCeiling;
+			int nCeiling = (float)(ScreenHeight() / 2.0) - ScreenHeight() / ((float)fDistToWall);
+			int nFloor = ScreenHeight() - nCeiling;
 
 			short nShade = ' ';
 			// using extended ascii
@@ -204,46 +157,45 @@ int main() {
 
 			if (bBoundary)	nShade = ' ';
 
-			for (int y = 0; y < nScreenHeight; y++) {
+			for (int y = 0; y < ScreenHeight(); y++) {
 				if (y <= nCeiling) {
-					screen[y * nScreenWidth + x] = ' ';
+					Draw(x, y, L' ');
 				}
-				else if (y>nCeiling && y<= nFloor) {
-					screen[y * nScreenWidth + x] = nShade;
+				else if (y > nCeiling && y <= nFloor) {
+					Draw(x, y, nShade);
 				}
 				else // Floor
 				{
 					// Shade floor based on distance
-					float b = 1.0f - (((float)y - nScreenHeight / 2.0f) / ((float)nScreenHeight / 2.0f));
+					float b = 1.0f - (((float)y - ScreenHeight() / 2.0f) / ((float)ScreenHeight() / 2.0f));
 					if (b < 0.25)		nShade = '#';
 					else if (b < 0.5)	nShade = 'x';
 					else if (b < 0.75)	nShade = '.';
 					else if (b < 0.9)	nShade = '-';
 					else				nShade = ' ';
-					screen[y * nScreenWidth + x] = nShade;
+					Draw(x, y, nShade);
 				}
 			}
 		}
 
-		if (bJustShot) {
-			drawMuzzleFlash(screen);
-		}
+		// Display Map & Player
+		for (int nx = 0; nx < nMapWidth; nx++)
+			for (int ny = 0; ny < nMapWidth; ny++)
+				Draw(nx + 1, ny + 1, map[ny * nMapWidth + nx]);
+		Draw(1 + (int)fPlayerX, 1 + (int)fPlayerY, L'P');
 
-		// Display Stats
-		swprintf_s(screen, 40, L"X=%3.2f, Y=%3.2f, A=%3.2f FPS=%3.2f", fPlayerX, fPlayerY, fPlayerA, 1.0f / fElapsedTime);
+		return true;
+	}
+};
 
-		// display map
-		for (int nx = 0; nx < nMapWidth; nx++) {
-			for (int ny = 0; ny < nMapWidth; ny++) {
-				screen[(ny + 1) * nScreenWidth + nx] = map[ny * nMapWidth + nx];
-			}
-		}
-		screen[((int)fPlayerY + 1) * nScreenWidth + (int)fPlayerX] = 'P';
+int main() {
+	Ultimate_FPS game;
 
-		// setting last char of screen to escape char
-		screen[nScreenWidth * nScreenHeight - 1] = '\0';
-		WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0, 0 }, &dwBytesWritten);
+	if (!game.ConstructConsole(240, 160, 4, 4)) {
+		std::wcerr << L"Console construction failed!" << endl;
+		return -1;
 	}
 
+	game.Start();
 	return 0;
 }
